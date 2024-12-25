1229a908156c42c68a895ab60395135a06699eb2
serpil-sarioglu <serpilsarioglu81@gmail.com>
2024-12-25 21:28:35 +0300

    ...

--- CSharpEgitimKampi301.PresentationLayer/FrmProduct.Designer.cs
+++ CSharpEgitimKampi301.PresentationLayer/FrmProduct.Designer.cs
@@ -29,6 +29,15 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnList2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtProductPrice = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtProductStock = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.btnGetById = new System.Windows.Forms.Button();
             this.btnUpdate = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
@@ -41,15 +50,6 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtProductId = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtProductStock = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txtProductPrice = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.txtDescription = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.cmbCategory = new System.Windows.Forms.ComboBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.btnList2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -58,55 +58,135 @@
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(216)))), ((int)(((byte)(224)))));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(355, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(355, 9);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(823, 614);
+            this.dataGridView1.Size = new System.Drawing.Size(810, 551);
             this.dataGridView1.TabIndex = 25;
+            // 
+            // btnList2
+            // 
+            this.btnList2.Location = new System.Drawing.Point(242, 339);
+            this.btnList2.Name = "btnList2";
+            this.btnList2.Size = new System.Drawing.Size(100, 40);
+            this.btnList2.TabIndex = 76;
+            this.btnList2.Text = "Listele 2";
+            this.btnList2.UseVisualStyleBackColor = true;
+            this.btnList2.Click += new System.EventHandler(this.btnList2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 169);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(123, 18);
+            this.label7.TabIndex = 75;
+            this.label7.Text = "Ürün Açıklaması :";
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(128, 125);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(219, 26);
+            this.cmbCategory.TabIndex = 60;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(128, 166);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(219, 123);
+            this.txtDescription.TabIndex = 61;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(55, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 18);
+            this.label6.TabIndex = 74;
+            this.label6.Text = "Kategori :";
+            // 
+            // txtProductPrice
+            // 
+            this.txtProductPrice.Location = new System.Drawing.Point(128, 96);
+            this.txtProductPrice.Name = "txtProductPrice";
+            this.txtProductPrice.Size = new System.Drawing.Size(219, 24);
+            this.txtProductPrice.TabIndex = 59;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 18);
+            this.label5.TabIndex = 73;
+            this.label5.Text = "Ürün Fiyat :";
+            // 
+            // txtProductStock
+            // 
+            this.txtProductStock.Location = new System.Drawing.Point(128, 67);
+            this.txtProductStock.Name = "txtProductStock";
+            this.txtProductStock.Size = new System.Drawing.Size(219, 24);
+            this.txtProductStock.TabIndex = 58;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 18);
+            this.label4.TabIndex = 72;
+            this.label4.Text = "Ürün Stok :";
             // 
             // btnGetById
             // 
-            this.btnGetById.Location = new System.Drawing.Point(130, 581);
+            this.btnGetById.Location = new System.Drawing.Point(128, 520);
             this.btnGetById.Name = "btnGetById";
-            this.btnGetById.Size = new System.Drawing.Size(219, 45);
-            this.btnGetById.TabIndex = 13;
+            this.btnGetById.Size = new System.Drawing.Size(214, 40);
+            this.btnGetById.TabIndex = 68;
             this.btnGetById.Text = "ID\'ye Göre Getir";
             this.btnGetById.UseVisualStyleBackColor = true;
+            this.btnGetById.Click += new System.EventHandler(this.btnGetById_Click);
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(130, 528);
+            this.btnUpdate.Location = new System.Drawing.Point(128, 475);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(219, 45);
-            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Size = new System.Drawing.Size(214, 40);
+            this.btnUpdate.TabIndex = 67;
             this.btnUpdate.Text = "Güncelle";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(130, 475);
+            this.btnDelete.Location = new System.Drawing.Point(128, 429);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(219, 45);
-            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Size = new System.Drawing.Size(214, 40);
+            this.btnDelete.TabIndex = 66;
             this.btnDelete.Text = "Sil";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(130, 422);
+            this.btnAdd.Location = new System.Drawing.Point(128, 384);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(219, 45);
-            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Size = new System.Drawing.Size(214, 40);
+            this.btnAdd.TabIndex = 65;
             this.btnAdd.Text = "Ekle";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnList
             // 
-            this.btnList.Location = new System.Drawing.Point(130, 369);
+            this.btnList.Location = new System.Drawing.Point(128, 339);
             this.btnList.Name = "btnList";
-            this.btnList.Size = new System.Drawing.Size(101, 45);
-            this.btnList.TabIndex = 9;
+            this.btnList.Size = new System.Drawing.Size(100, 40);
+            this.btnList.TabIndex = 64;
             this.btnList.Text = "Listele";
             this.btnList.UseVisualStyleBackColor = true;
             this.btnList.Click += new System.EventHandler(this.btnList_Click);
@@ -114,10 +194,10 @@
             // rdbPassive
             // 
             this.rdbPassive.AutoSize = true;
-            this.rdbPassive.Location = new System.Drawing.Point(220, 322);
+            this.rdbPassive.Location = new System.Drawing.Point(218, 301);
             this.rdbPassive.Name = "rdbPassive";
             this.rdbPassive.Size = new System.Drawing.Size(62, 22);
-            this.rdbPassive.TabIndex = 8;
+            this.rdbPassive.TabIndex = 63;
             this.rdbPassive.TabStop = true;
             this.rdbPassive.Text = "Pasif";
             this.rdbPassive.UseVisualStyleBackColor = true;
@@ -125,10 +205,10 @@
             // rdbActive
             // 
             this.rdbActive.AutoSize = true;
-            this.rdbActive.Location = new System.Drawing.Point(133, 322);
+            this.rdbActive.Location = new System.Drawing.Point(131, 301);
             this.rdbActive.Name = "rdbActive";
             this.rdbActive.Size = new System.Drawing.Size(57, 22);
-            this.rdbActive.TabIndex = 7;
+            this.rdbActive.TabIndex = 62;
             this.rdbActive.TabStop = true;
             this.rdbActive.Text = "Aktif";
             this.rdbActive.UseVisualStyleBackColor = true;
@@ -136,126 +216,51 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 324);
+            this.label3.Location = new System.Drawing.Point(6, 303);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(120, 18);
-            this.label3.TabIndex = 17;
+            this.label3.TabIndex = 71;
             this.label3.Text = "Kategori Durum :";
             // 
             // txtProductName
             // 
-            this.txtProductName.Location = new System.Drawing.Point(130, 45);
+            this.txtProductName.Location = new System.Drawing.Point(128, 38);
             this.txtProductName.Name = "txtProductName";
             this.txtProductName.Size = new System.Drawing.Size(219, 24);
-            this.txtProductName.TabIndex = 2;
+            this.txtProductName.TabIndex = 57;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(55, 45);
+            this.label2.Location = new System.Drawing.Point(54, 39);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(72, 18);
-            this.label2.TabIndex = 15;
+            this.label2.TabIndex = 70;
             this.label2.Text = "Ürün Adı :";
             // 
             // txtProductId
             // 
-            this.txtProductId.Location = new System.Drawing.Point(130, 13);
+            this.txtProductId.Location = new System.Drawing.Point(128, 9);
             this.txtProductId.Name = "txtProductId";
             this.txtProductId.Size = new System.Drawing.Size(219, 24);
-            this.txtProductId.TabIndex = 1;
+            this.txtProductId.TabIndex = 56;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(65, 13);
+            this.label1.Location = new System.Drawing.Point(63, 10);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(63, 18);
-            this.label1.TabIndex = 14;
+            this.label1.TabIndex = 69;
             this.label1.Text = "Ürün Id :";
-            // 
-            // txtProductStock
-            // 
-            this.txtProductStock.Location = new System.Drawing.Point(130, 79);
-            this.txtProductStock.Name = "txtProductStock";
-            this.txtProductStock.Size = new System.Drawing.Size(219, 24);
-            this.txtProductStock.TabIndex = 3;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(47, 79);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(83, 18);
-            this.label4.TabIndex = 26;
-            this.label4.Text = "Ürün Stok :";
-            // 
-            // txtProductPrice
-            // 
-            this.txtProductPrice.Location = new System.Drawing.Point(130, 113);
-            this.txtProductPrice.Name = "txtProductPrice";
-            this.txtProductPrice.Size = new System.Drawing.Size(219, 24);
-            this.txtProductPrice.TabIndex = 4;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(45, 114);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(83, 18);
-            this.label5.TabIndex = 28;
-            this.label5.Text = "Ürün Fiyat :";
-            // 
-            // txtDescription
-            // 
-            this.txtDescription.Location = new System.Drawing.Point(130, 184);
-            this.txtDescription.Multiline = true;
-            this.txtDescription.Name = "txtDescription";
-            this.txtDescription.Size = new System.Drawing.Size(219, 123);
-            this.txtDescription.TabIndex = 6;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(56, 148);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(71, 18);
-            this.label6.TabIndex = 30;
-            this.label6.Text = "Kategori :";
-            // 
-            // cmbCategory
-            // 
-            this.cmbCategory.FormattingEnabled = true;
-            this.cmbCategory.Location = new System.Drawing.Point(130, 146);
-            this.cmbCategory.Name = "cmbCategory";
-            this.cmbCategory.Size = new System.Drawing.Size(219, 26);
-            this.cmbCategory.TabIndex = 5;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(3, 186);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(123, 18);
-            this.label7.TabIndex = 33;
-            this.label7.Text = "Ürün Açıklaması :";
-            // 
-            // btnList2
-            // 
-            this.btnList2.Location = new System.Drawing.Point(248, 369);
-            this.btnList2.Name = "btnList2";
-            this.btnList2.Size = new System.Drawing.Size(101, 45);
-            this.btnList2.TabIndex = 34;
-            this.btnList2.Text = "Listele 2";
-            this.btnList2.UseVisualStyleBackColor = true;
-            this.btnList2.Click += new System.EventHandler(this.btnList2_Click);
             // 
             // FrmProduct
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(123)))), ((int)(((byte)(236)))));
-            this.ClientSize = new System.Drawing.Size(1184, 639);
+            this.ClientSize = new System.Drawing.Size(1170, 564);
             this.Controls.Add(this.btnList2);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.cmbCategory);
@@ -265,7 +270,6 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.txtProductStock);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnGetById);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.btnDelete);
@@ -278,9 +282,11 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtProductId);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Name = "FrmProduct";
             this.Text = "Ürün";
+            this.Load += new System.EventHandler(this.FrmProduct_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -290,6 +296,15 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnList2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtProductPrice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtProductStock;
+        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnGetById;
         private System.Windows.Forms.Button btnUpdate;
         private System.Windows.Forms.Button btnDelete;
@@ -302,14 +317,5 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtProductId;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtProductStock;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox txtProductPrice;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox txtDescription;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.ComboBox cmbCategory;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Button btnList2;
     }
 }
